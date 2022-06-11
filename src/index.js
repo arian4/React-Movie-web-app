3ccc13352289e1da4fabc0c78f9b7c0ad48532f4
Arian_hajiaghaei <Arian313h@gmail.com>
2022-06-11 10:49:31 +0430

    deploy bugs fixed

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppContextProvider } from './context/AppContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppContextProvider>
         <App />
       </AppContextProvider>
       
-    </BrowserRouter>
+    </HashRouter>
     
   </React.StrictMode>
 );
